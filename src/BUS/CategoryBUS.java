b056c2de53bcf183d0884b66ebde2c0966a5e537
LN-1106 <workingnly1234@gmail.com>
2026-03-04 08:10:26 +0700

    BUS

---
+++ src/BUS/CategoryBUS.java
@@ -0,0 +1,26 @@
+package BUS;
+
+import DTO.CategoryDTO;
+import DAO.CategoryDAO;
+import java.sql.*;
+import java.util.ArrayList;
+
+import DAO.CategoryDAO;
+public class CategoryBUS {
+    private CategoryDAO categoryDAO;
+    public CategoryBUS() {
+        categoryDAO = new CategoryDAO();
+    }
+    public ArrayList<CategoryDTO> getAllCategories() {
+        return categoryDAO.getAllCategory();
+    }
+    public boolean addCategory(CategoryDTO category) {
+        if(category.getName() == null || category.getName().isEmpty()){
+            System.out.println("Tên danh mục không được để trống.");
+            return false;
+        }
+        
+        return categoryDAO.addCategory(category);
+    }
+    
+}
